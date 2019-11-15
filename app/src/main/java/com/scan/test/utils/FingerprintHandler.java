fd851417fa75d6cf569c7f3815b5adb8237e860e
quyenph05743 <vanan3045@gmail.com>
2019-11-15 13:42:53 +0700

    commit

---
+++ app/src/main/java/com/scan/test/utils/FingerprintHandler.java
@@ -0,0 +1,75 @@
+package com.scan.test.utils;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
+import android.os.CancellationSignal;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+
+import com.scan.test.ui.MainActivity;
+
+@RequiresApi(api = Build.VERSION_CODES.M)
+public class FingerprintHandler extends FingerprintManager.AuthenticationCallback {
+    private CancellationSignal cancellationSignal;
+    private Context context;
+
+    public FingerprintHandler(Context mContext) {
+        context = mContext;
+    }
+
+    //Implement the startAuth method, which is responsible for starting the fingerprint authentication process//
+
+    public void startAuth(FingerprintManager manager, FingerprintManager.CryptoObject cryptoObject) {
+
+        cancellationSignal = new CancellationSignal();
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        manager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
+    }
+
+    @Override
+    //onAuthenticationError is called when a fatal error has occurred. It provides the error code and error message as its parameters//
+
+    public void onAuthenticationError(int errMsgId, CharSequence errString) {
+
+        //I’m going to display the results of fingerprint authentication as a series of toasts.
+        //Here, I’m creating the message that’ll be displayed if an error occurs//
+
+        Toast.makeText(context, "Lỗi xác thực\n" + errString, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+
+    //onAuthenticationFailed is called when the fingerprint doesn’t match with any of the fingerprints registered on the device//
+
+    public void onAuthenticationFailed() {
+        Toast.makeText(context, "Dấu vân tay không đúng!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+
+    //onAuthenticationHelp is called when a non-fatal error has occurred. This method provides additional information about the error,
+    //so to provide the user with as much feedback as possible I’m incorporating this information into my toast//
+    public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
+        Toast.makeText(context, "Trợ giúp xác thực\n" + helpString, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+
+    //onAuthenticationSucceeded is called when a fingerprint has been successfully matched to one of the fingerprints stored on the user’s device//
+    public void onAuthenticationSucceeded(
+            FingerprintManager.AuthenticationResult result) {
+        Intent intent = new Intent(context, MainActivity.class);
+        context.startActivity(intent);
+        Toast.makeText(context, "Thành công!", Toast.LENGTH_LONG).show();
+    }
+
+}
